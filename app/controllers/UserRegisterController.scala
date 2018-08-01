e9f10195a1068e6f10204dece0c832d52fac3fee
captain <captain@dripower.com>
2018-08-01 10:40:15 +0800

    update ajax and others

--- app/controllers/UserRegisterController.scala
+++ app/controllers/UserRegisterController.scala
@@ -1,22 +1,24 @@
 package controllers
 
+import akka.actor.Status.Success
 import javax.inject.Inject
 import models.User
 import play.api.mvc._
 import services.UserService
+
 import scala.concurrent.ExecutionContext.Implicits.global
 import play.api.data._
 import play.api.data.Forms._
 import play.api.data.validation.Constraints._
+
 import scala.concurrent.Future
-import play.api.mvc.Result
 
 class UserRegisterController @Inject() (cc: ControllerComponents) extends AbstractController(cc){
 	def register = Action {
 		Ok(views.html.UserRegister("用户注册"))
 	}
 
-	def doRegister = Action.async { implicit request =>
+	def doRegister = Action.async(parse.form(userForm)) { implicit request =>
 		val (x, userName, city, password) = request.body
 //		println(userData)
 //		val newUser = User(userData.email, userData.userName,userData.city,userData.password)
@@ -27,12 +29,11 @@ class UserRegisterController @Inject() (cc: ControllerComponents) extends Abstra
 			password = password
 		)
 		val userService = new UserService
-		val fut = for {
+		val r = for {
 			id <- userService.addUser(user)
 			userR <- userService.getUser(id)
 		} yield userR
-		fut.map(r => Ok(r))
-		// OK("captain")
+		OK("captain")
 	}
 
 	val userForm = Form(
